97a9e914063a882318b3d7a2bac2649268548b40
juliohsu <tnt700900@gmail.com>
2025-01-10 23:33:51 -0300

    ordere the codes and undestanding with comments

--- lr_qahan/noisy_data.py
+++ lr_qahan/noisy_data.py
@@ -12,19 +12,17 @@ np.random.seed(42)
 X = torch.linspace(0, 10, 100).reshape(-1, 1)
 y = X * 2.5 + 7.0 + torch.rand_like(X) * 2
 
-# create noise for the input dataset
+# create noisy features for the dataset
 X_noise = torch.randn(100, 2) * 5
 X_unnecessary = torch.linspace(10, 20, 100).reshape(-1, 1) * torch.randn(100, 1)
-
 X_combined = torch.cat([X, X_noise, X_unnecessary], dim=1)
-X_interaction = X ** 2 + X * 3
 
-# create outlier for the outpt
+# create one more feature polynomial functions, which can be insightful
+X_final = torch.cat([X_combined, X ** 2 + X * 3], dim=1)
+
+# create outlier for 10th output sequences to mimic real world incertain
 y[::10] += torch.randn(10, 1) * 20
-
-# combine two dataset
-X_final = torch.cat([X_combined, X_interaction], dim=1)
-print(X_final)
+print(y)
 
 # visualize the result dataset
 plt.scatter(X_final[:, 0].numpy(), y.numpy(), label="Noisy dataset")
